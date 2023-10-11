ce6b8ff2c5d0e40bb34e42c1ed2e817bdaaa7adf
Phil Hunter <philhunter93@live.co.uk>
2023-10-11 09:57:19 +0100

    Update non_generalized_zero_inflated_poisson.py

--- Python/non_generalized_zero_inflated_poisson.py
+++ Python/non_generalized_zero_inflated_poisson.py
@@ -2,21 +2,24 @@ from sklearn.base import BaseEstimator, RegressorMixin
 from sklearn.linear_model import LogisticRegression, LinearRegression
 import numpy as np
 from scipy.stats import poisson
-from scipy.optimize import minimize
+from scipy.optimize import minimize, least_squares
+import statsmodels 
+import statsmodels.api as sm
+import statsmodels.discrete.count_model as reg_models
+from sklearn.utils.estimator_checks import check_estimator
+import warnings
+
 
 def dpois(x, lamda):
     return np.exp(-lamda) * (lamda ** x) / np.math.factorial(x)
-
-def ppois(x, lamda):
-    return poisson.cdf(x, lamda)
 
 def zi_poisson(pars, trunc, _weights, _X, _y, _y0, _y1,  _Z, _kx, _kz, _offset_x, _offset_z):
     mean_val = np.exp(np.dot(X, pars[:_kx]) + _offset_x)
     if trunc:
         phi_val = np.repeat(0, len(mean_val))
     else:
-        i1 = _kx + 1
-        i2 = _kz + _kz
+        i1 = _kx 
+        i2 = _kz + _kz - 1 
         phi_val = np.exp(np.dot(_Z, pars[i1:i2]) + _offset_z)
     log_lik_0 = np.log(phi_val + np.exp(np.log(1 - phi_val) - mean_val))
     log_lik_1 = np.log(1 - phi_val) + np.log(dpois(_y, mean_val))
@@ -30,29 +33,30 @@ def cg_poisson(pars, _weights, _X, _y,  _y1,  _Z, _kx, _kz, _offset_x, _offset_z
     full_eta = np.dot(_X, pars[:_kx]) + _offset_x
     eta = full_eta[_y1]
     mu = np.exp(eta)
-    out = (_y[_y1] - mu - np.exp(np.log(poisson.cdf(0, mu)) - np.log(1 - poisson.cdf(0, mu)) + eta)) * _weights[_y1] * _X[_y1]
-    print(np.sum(out))
-    return np.sum(out)
+    out = (_y[_y1] - mu - np.exp(np.log(poisson.cdf(0, mu)) - np.log(1 - poisson.cdf(0, mu)) + eta)) * _weights[_y1] * _X[_y1]    
+    return np.sum(out,axis=0)[1]
 
 def g_poisson(pars, _weights, _X, _y,  _y1,  _Z, _kx, _kz, _offset_x, _offset_z):
-    print("g is running")
     eta = np.dot(_X, pars[:_kx]) + _offset_x
-    print("g eta is running")
-
     mu = np.exp(eta)
-    print("g mu is running")
-
-    i1 = _kx + 1
-    i2 = _kz + _kz
-    eta_z = np.exp(np.dot(_Z, pars[i1:i2]) + _offset_z)
-
-    mu_z = np.exp(eta_z)
+    i1 = _kx 
+    i2 = _kx + _kz - 1
+    if i1 is i2:
+        pars_z = np.array(pars[i1-1])
+    else:
+        pars_z = pars[i1:i2]  
+    eta_z = np.exp(np.dot(_Z, pars_z) )
+    N = len(eta_z)
+    mu_z = np.max(np.array([np.exp(eta_z), np.repeat(2.220446e-10, N).reshape(N,1)]), axis=0)
     clog_dens = -mu
-    dens = mu_z * (1 - _y1) + np.exp(np.log(1-mu_z)+clog_dens + np.log(mu))
-    print(dens)
-    res_count = np.where(_y1, _y - mu, -np.exp(-np.log(dens) + np.log(1-mu_z)) + clog_dens + np.log(mu))
-    res_zero = np.where(_y1, -1 / (1 - mu_z) * np.exp(eta_z), (np.exp(eta_z) - np.exp(clog_dens) * np.exp(eta_z)) / dens)
-    return np.array([res_count * _weights * _X], [res_zero * _weights * _Z]).sum()#axis=0)
+    dens = mu_z * (1 - _y1.astype(int)) + np.exp(np.log(1 - -mu_z) + clog_dens)    
+    res_count = np.where(_y1, _y - mu, -np.exp(-np.log(dens) + np.log(1- -mu_z)) + clog_dens + np.log(mu))[0]
+    res_zero = np.where(_y1, -1 / (1 - mu_z) * np.exp(eta_z), (np.exp(eta_z) - np.exp(clog_dens) * np.exp(eta_z)) / dens)[0]
+    p1 = res_count* _weights * _X.reshape(-1)
+    p2 = res_zero * _weights * _Z.reshape(-1)
+    p_array_out = np.array([p1, p2])
+    p_array_sum = p_array_out.sum(axis=1)
+    return p_array_sum[1]
 
 def non_generalized_zero_inflated_poisson(X, y):
     """
@@ -61,8 +65,8 @@ def non_generalized_zero_inflated_poisson(X, y):
 
     """
     _y = y
-    _X = X.copy()
-    _Z = X
+    _X = X
+    _Z = _X
     N = len(y)
     _kx = min(X.shape)
     _kz = _kx
@@ -72,7 +76,42 @@ def non_generalized_zero_inflated_poisson(X, y):
     _offset_x = np.repeat(0, N)
     _offset_z = np.repeat(0, N)
     zero_model = LogisticRegression(penalty = "none").fit(_Z, _y0.astype(int))
-    pars = LinearRegression().fit(_X[_y1], y[_y1]).coef_
-    fitted = minimize(fun=cg_poisson, x0=pars, args=(_weights, _X, _y,  _y1,  _Z, _kx, _kz, _offset_x, _offset_z),  jac=g_poisson )
-    # not yet complete
-    return fitted
+    #pars = LinearRegression().fit(_X[_y1], y[_y1]).coef_
+    warnings.filterwarnings("ignore")
+    count_model = reg_models.Poisson(endog=y,exog=_X.reshape(-1, 1) ).fit(disp=0)
+    count_model_params = np.append(reg_models.Poisson(endog=y,exog=_X.reshape(-1, 1) ).fit(disp=0).params, 0.1)
+    warnings.filterwarnings("always")
+    starts = dict({"Count":count_model, "Zero":zero_model})
+    starts_coefs = dict({"Count":count_model_params, "Zero":zero_model.coef_})
+    #fitted = minimize(fun=cg_poisson, x0=pars, args=(_weights, _X, _y,  _y1,  _Z, _kx, _kz, _offset_x, _offset_z), method="BFGS",  jac=g_poisson)
+    #fitted = least_squares(fun=cg_poisson, x0=pars, args=(_weights, _X, _y,  _y1,  _Z, _kx, _kz, _offset_x, _offset_z),   jac=g_poisson)
+    return starts #fitted
+
+class NonGeneralizedZeroInflatedPoisson(BaseEstimator, RegressorMixin):
+    def fit(self, X, y):
+        model = reg_models.ZeroInflatedGeneralizedPoisson(endog=y,exog= X)
+        self.fit = model.fit()
+        self.model = model
+        return self
+    def predict(self, X):
+        return self.fit.predict(X)
+
+
+class SMWrapper(BaseEstimator, RegressorMixin):
+    """ A universal sklearn-style wrapper for statsmodels regressors
+     Doesn't work for zero-inflated, yet """
+    def __init__(self, model_class, fit_intercept=True):
+        self.model_class = model_class
+        self.fit_intercept = fit_intercept
+    def fit(self, X, y):
+        if self.fit_intercept:
+            X = sm.add_constant(X)
+        self.model_ = self.model_class(y, X)
+        self.results_ = self.model_.fit()
+        return self
+    def predict(self, X):
+        if self.fit_intercept:
+            X = sm.add_constant(X)
+        return self.results_.predict(X)
+
+#check_estimator(SMWrapper(sm.ZeroInflatedGeneralizedPoisson))
